c5e5afe9693e79f207a1c95207e492437d3eec98
x-dr <79109673+x-dr@users.noreply.github.com>
2026-05-10 10:52:40 +0800

    Bump version for UI auto refresh fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "cn.tryxd.powermanager"
         minSdk = 26
         targetSdk = 33
-        versionCode = 4
-        versionName = "1.3.0"
+        versionCode = 5
+        versionName = "1.3.1"
     }
 
     compileOptions {
